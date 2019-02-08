c31bfb84daf9b1b34394c1777587b7a902eb47e8
qorrect <charlie.fats@gmail.com>
2019-02-08 16:46:39 -0600

    Refactoring.

--- app/src/dl/js/checkbox.js
+++ app/src/dl/js/checkbox.js
@@ -33,20 +33,21 @@
 
 // @require button.js
 
-DEFINE_CLASS("DlCheckbox", DlAbstractButton, function(D, P) {
+DEFINE_CLASS('DlCheckbox', DlAbstractButton, function (D, P) {
 
-        D.DEFAULT_ARGS = {
-	        _classes : [ "classes", { active    : "DlCheckbox-active",
-				          hover     : "DlCheckbox-hover",
-				          checked   : "DlCheckbox-1",
-				          unchecked : "DlCheckbox-0",
-				          empty     : "DlCheckbox-empty",
-				          disabled  : "DlCheckbox-disabled"
-				        } ]
-        };
+    D.DEFAULT_ARGS = {
+        _classes: ['classes', {
+            active: 'DlCheckbox-active',
+            hover: 'DlCheckbox-hover',
+            checked: 'DlCheckbox-1',
+            unchecked: 'DlCheckbox-0',
+            empty: 'DlCheckbox-empty',
+            disabled: 'DlCheckbox-disabled'
+        }]
+    };
 
-        D.FIXARGS = function(args) {
-                args.type = DlButton.TYPE.TWOSTATE;
-        };
+    D.FIXARGS = function (args) {
+        args.type = DlButton.TYPE.TWOSTATE;
+    };
 
 });
