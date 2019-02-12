ed03bca2c787f89318a9235022d28f89f8d7f445
qorrect <charlie.fats@gmail.com>
2019-02-11 21:47:41 -0600

    Fixes #45, adding refresh to app.

--- elbuild/index.js
+++ elbuild/index.js
@@ -1,12 +1,22 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, globalShortcut} = require('electron');
 
 function main() {
     // Create the browser window.
     const win = new BrowserWindow({width: 800, height: 600});
 
     win.setMenu(null);
+    globalShortcut.register('f5', () => {
+        console.log('f5 is pressed');
+        win.reload();
+    });
+    globalShortcut.register('CommandOrControl+R', () => {
+        console.log('CommandOrControl+R is pressed');
+        win.reload();
+    });
     // and load the index.html of the app.
     win.loadFile('../app/index.html');
+    win.webContents.openDevTools();
+
 }
 
 app.on('ready', main);
